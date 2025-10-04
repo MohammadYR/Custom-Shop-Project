ff32a7cdd0cbf8d3c4a0dc1ac40f2308c67eb6b0
Mohammad Yousefi <95502880+MohammadYR@users.noreply.github.com>
2025-10-05 02:52:23 +0330

    refactor(auth,core): switch to UUID PKs; load .env in manage.py; use psycopg[binary]

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+class SoftDeleteAdminMixin(admin.ModelAdmin):
+    actions = ["soft_delete_selected", "restore_selected"]
+
+    @admin.action(description="Soft delete selected")
+    def soft_delete_selected(self, request, queryset):
+        
+        queryset.delete()  # soft
+
+    @admin.action(description="Restore selected")
+    def restore_selected(self, request, queryset):
+        queryset.restore()
+
+# استفاده:
+# @admin.register(YourModel)
+# class YourModelAdmin(SoftDeleteAdminMixin): pass
